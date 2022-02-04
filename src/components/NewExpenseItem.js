e479656077b89a38564a1cdc6d6b631e062e9544
Huaicheng-D <1651681700@qq.com>
2022-02-03 22:54:23 -0800

    Commit for src folder

---
+++ src/components/NewExpenseItem.js
@@ -0,0 +1,10 @@
+import "./NewExpenseItem.css";
+
+function NewExpenseItem() {
+  return (
+    <div className="newExpense">
+      <button>add new expense here</button>
+    </div>
+  );
+}
+export default NewExpenseItem;
