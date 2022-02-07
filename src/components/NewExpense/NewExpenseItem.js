71d6df0528fe82b71a9c81a387321bd1448a85a0
Huaicheng-D <1651681700@qq.com>
2022-02-06 23:44:19 -0800

    Implemented NewExpenseForm.js form template

---
+++ src/components/NewExpense/NewExpenseItem.js
@@ -0,0 +1,11 @@
+import "./NewExpenseItem.css";
+import ExpenseForm from "./ExpenseForm";
+
+function NewExpenseItem() {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
+    </div>
+  );
+}
+export default NewExpenseItem;
